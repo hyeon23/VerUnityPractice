1ce534620a0ac504251f47f9b4e5843bf9f551be
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-01-08 01:17:25 +0900

    Input2 & Input3 Update

---
+++ Assets/Scene6[Animation]/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private bool isFire;
+    private Vector3 direction;
+    [SerializeField]
+    private float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFire)
+        {
+            transform.Translate(direction * Time.deltaTime * speed);
+        }
+    }
+
+    public void Fire(Vector3 dir)
+    {
+        direction = dir;
+        isFire = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Bullet>() == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scene6[Animation]/unity-key-input-character-move/Assets/Scripts/SimpleCharacterController.cs
+++ Assets/Scene6[Animation]/unity-key-input-character-move/Assets/Scripts/SimpleCharacterController.cs
@@ -6,10 +6,22 @@ public class SimpleCharacterController : MonoBehaviour
 {
     [SerializeField]
     private Animator animator;
-    // Start is called before the first frame update
-    void Start()
+    Camera characterCamera;
+
+    public GameObject bulletPrefab;
+
+    void Fire()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 firePos = transform.position + animator.transform.forward + new Vector3(0f, 0.5f, 0f);
+            var bullet = Instantiate(bulletPrefab, firePos, Quaternion.identity).GetComponent<Bullet>();
+            bullet.Fire(animator.transform.forward);
+        }
+    }
+    private void Awake()
+    {
+        characterCamera = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
@@ -34,6 +46,7 @@ public class SimpleCharacterController : MonoBehaviour
         //{
         //    moveZ += 1f;
         //}
+        //transform.Translate(new Vector3(moveX, 0f, moveZ).normalized * Time.deltaTime * 5f);
 
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
@@ -41,12 +54,31 @@ public class SimpleCharacterController : MonoBehaviour
         Vector3 moveVector = new Vector3(moveX, 0f, moveZ);
         bool isMove = moveVector.magnitude > 0;
         animator.SetBool("isMove", isMove);
-        if (isMove)
-        {
-            animator.transform.forward = moveVector;
-        }
-
-
+        //if (isMove)
+        //{
+        //    animator.transform.forward = moveVector;
+        //}
+        LookMouseCursor();
+        Fire();
+        Zoom();
         transform.Translate(new Vector3(moveX, 0f, moveZ).normalized * Time.deltaTime * 5f);
+    }
+
+    public void LookMouseCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            Vector3 mouseDir = new Vector3(hit.point.x, transform.position.y, hit.point.z) - transform.position;
+            animator.transform.forward = mouseDir;
+        }
+        
+    }
+
+    public void Zoom()
+    {
+        var scroll = Input.mouseScrollDelta;
+        characterCamera.fieldOfView = Mathf.Clamp(characterCamera.fieldOfView - scroll.y, 30f, 70f);
     }
 }
