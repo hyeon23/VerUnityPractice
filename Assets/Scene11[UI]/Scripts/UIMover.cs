866771b3c75ccce10d83ed7f51a8267d42b525ab
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-01-09 00:21:43 +0900

    Animation Layer Update

--- Assets/Scene11[UI]/Scripts/UIMover.cs
+++ Assets/Scene11[UI]/Scripts/UIMover.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private RectTransform rectTransform;
+
+    private void Awake()
     {
-        
+        //현재 스크립트가 적용된 오브젝트의 RectTransform
+        rectTransform = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //UI 위치조정
+        if (Input.GetMouseButton(0))
+        {
+            rectTransform.anchoredPosition = Input.mousePosition; 
+        }
+
+        //UI 크기조정
+        //-transform.localScale을 통해 조작하는 것과 유사한 결과
+        //-but IOS 버전에서 문제를 발생시키는 것 외에 여러 문제가 발생해 UI에선 RectTransform 사용하셈
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            rectTransform.sizeDelta = rectTransform.sizeDelta * 1.1f;
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            rectTransform.sizeDelta = rectTransform.sizeDelta * 0.9f;
+        }
     }
 }
